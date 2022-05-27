5ee9ff84928a5874245304374303644f5384b718
Andrei Chigileichik <andrei.chigileichik@intexsoft.by>
2022-05-27 18:44:06 +0300

    Add functional component hero and services (5, 6)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
-import {Hero} from "../hero";
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-  heroes = HEROES;
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
